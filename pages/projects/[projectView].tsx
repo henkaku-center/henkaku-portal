7e75e9f9a2c4c6a3aa0b5a909da69b2f3f790b49
cohurin <neila@users.noreply.github.com>
2022-04-27 11:08:51 +0900

    fix

--- pages/projects/[projectView].tsx
+++ pages/projects/[projectView].tsx
@@ -1,30 +1,31 @@
 import Link from "next/link";
 import AppLayout from "@lib/components/Layouts/AppLayout";
 
-export default function project(pageProps){
+export default function project(){
     return (
         <AppLayout title="">
             <div>
-                {pageProps}
+                {/* {pageProps} */}
+                <h1>Project component</h1>
             </div>
         </AppLayout>
     )
 }
 
-export async function getStaticPaths() {
+// export async function getStaticPaths() {
 
-    const p = {params: null}
+//     const p = {params: null}
 
-    return { paths: p, fallback: false }
-}
+//     return { paths: p, fallback: false }
+// }
 
 
-export async function getStaticProps( context ) {
+// export async function getStaticProps( context ) {
 
-    const { params } = context;
-    const pageProps = ""
+//     const { params } = context;
+//     const pageProps = ""
 
-    return { 
-        props: pageProps
-    };
-}
+//     return { 
+//         props: pageProps
+//     };
+// }
