d1114ba27396ac4dda50f2c9b932b387962bab1e
Masumi Kawasaki <geeknees@gmail.com>
2022-04-28 00:17:03 +0900

    rebuild foundation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,7 @@
-import React from 'react'
-import type { ExtendedAppProps } from '@lib/types'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { SessionProvider } from 'next-auth/react'
-import '@lib/styles/index.css'
-import WithAuth from '@lib/auth/WithAuth'
+import '../styles/globals.css'
 
-export const queryClient = new QueryClient()
-
-function MyApp({
-    Component,
-    pageProps: { session, ...pageProps },
-}: ExtendedAppProps) {
-    return (
-        <SessionProvider session={session} refetchInterval={5 * 60}>
-            <QueryClientProvider client={queryClient}>
-                {Component.auth ? (
-                    <WithAuth options={Component.auth}>
-                        <Component {...pageProps} />
-                    </WithAuth>
-                ) : (
-                    <Component {...pageProps} />
-                )}
-            </QueryClientProvider>
-        </SessionProvider>
-    )
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
 
 export default MyApp
