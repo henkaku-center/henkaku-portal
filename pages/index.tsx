5e6da4f914491d22c988046ce7874f99886975c2
Masumi Kawasaki <geeknees@gmail.com>
2022-04-28 01:19:42 +0900

    feat: update top ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, signOut, useSession } from 'next-auth/client'
+import { chakra } from '@chakra-ui/react'
+import { Center, VStack, Heading, Text, Box } from '@chakra-ui/react'
 import Guilds from '../components/Guilds'
 
 export default function Page() {
@@ -7,9 +9,31 @@ export default function Page() {
   return (
     <>
       {!session && (
-        <>
-          <button onClick={signIn}>Sign in</button>
-        </>
+        <VStack h={500} justify='center'>
+          <Heading as='h1' size='4xl'>
+            HENKAKU
+          </Heading>
+          <Text fontSize='5xl' color='rgba(120,120,120,0.8)'>
+            Discord Community Portal
+          </Text>
+
+          <Box
+            as='button'
+            px={6}
+            h={8}
+            shadow='md'
+            color='black'
+            fontWeight='bold'
+            borderRadius='full'
+            bgGradient='linear(to-r, #6ee7b7, #60a5fa)'
+            _hover={{
+              bgGradient: 'linear(to-r,#a78bfa, #f59e0b)',
+            }}
+            onClick={signIn}
+          >
+            login
+          </Box>
+        </VStack>
       )}
       {session && (
         <>
